90617cb8bb2b3948499bf3c9f601ad050d333434
EldarTem <temirov.e.i.21@gmail.com>
2023-04-14 01:20:35 +0300

    Добавление слайдера и фикс полоски

--- js/sctipt.js
+++ js/sctipt.js
@@ -12,3 +12,52 @@ for (let anchor of anchors) {
     });
   });
 }
+
+const slides = document.querySelectorAll('.slider img');
+const thumbnails = document.querySelectorAll('.slider-thumbnails img');
+const prevBtn = document.querySelector('.prev-slide');
+const nextBtn = document.querySelector('.next-slide');
+
+let currentSlide = 0;
+
+function showSlide() {
+  slides.forEach((slide, index) => {
+    if (index === currentSlide) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+  thumbnails.forEach((thumbnail, index) => {
+    if (index === currentSlide) {
+      thumbnail.classList.add('active');
+    } else {
+      thumbnail.classList.remove('active');
+    }
+  });
+}
+
+function prevSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide();
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide > slides.length - 1) {
+    currentSlide = 0;
+  }
+  showSlide();
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', () => {
+    currentSlide = index;
+    showSlide();
+  });
+});
